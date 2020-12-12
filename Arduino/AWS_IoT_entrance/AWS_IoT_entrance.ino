242b26e5bfcd4d954c250ce0ace50a51cbcea98b
jjuun0 <jh061993@gmail.com>
2020-12-12 19:38:17 +0900

    upload

---
+++ Arduino/AWS_IoT_entrance/AWS_IoT_entrance.ino
@@ -0,0 +1,240 @@
+/*
+  AWS IoT WiFi
+
+  This sketch securely connects to an AWS IoT using MQTT over WiFi.
+  It uses a private key stored in the ATECC508A and a public
+  certificate for SSL/TLS authetication.
+
+  It publishes a message every 5 seconds to arduino/outgoing
+  topic and subscribes to messages on the arduino/incoming
+  topic.
+
+  The circuit:
+  - Arduino MKR WiFi 1010 or MKR1000
+
+  The following tutorial on Arduino Project Hub can be used
+  to setup your AWS account and the MKR board:
+
+  https://create.arduino.cc/projecthub/132016/securely-connecting-an-arduino-mkr-wifi-1010-to-aws-iot-core-a9f365
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBearSSL.h>
+#include <ArduinoECCX08.h>
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h> // change to #include <WiFi101.h> for MKR1000
+
+#include "arduino_secrets.h"
+
+#define LED_1_PIN 5
+
+#include <ArduinoJson.h>
+#include "Led.h"
+
+/////// Enter your sensitive data in arduino_secrets.h
+const char ssid[]        = SECRET_SSID;
+const char pass[]        = SECRET_PASS;
+const char broker[]      = SECRET_BROKER;
+const char* certificate  = SECRET_CERTIFICATE;
+
+WiFiClient    wifiClient;            // Used for the TCP socket connection
+BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates with ECC508
+MqttClient    mqttClient(sslClient);
+
+unsigned long lastMillis = 0;
+
+Led led1(LED_1_PIN);
+
+#define trigPin 13                   // trigPin을 13으로 설정합니다.
+#define echoPin 12                // echoPin을 12로 설정합니다.
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  
+  pinMode(trigPin, OUTPUT);   // trigPin 핀을 출력핀으로 설정합니다.
+  pinMode(echoPin, INPUT);    // echoPin 핀을 입력핀으로 설정합니다.
+
+  if (!ECCX08.begin()) {
+    Serial.println("No ECCX08 present!");
+    while (1);
+  }
+
+  // Set a callback to get the current time
+  // used to validate the servers certificate
+  ArduinoBearSSL.onGetTime(getTime);
+
+  // Set the ECCX08 slot to use for the private key
+  // and the accompanying public certificate for it
+  sslClient.setEccSlot(0, certificate);
+
+  // Optional, set the client id used for MQTT,
+  // each device that is connected to the broker
+  // must have a unique client id. The MQTTClient will generate
+  // a client id for you based on the millis() value if not set
+  //
+  // mqttClient.setId("clientId");
+
+  // Set the message callback, this function is
+  // called when the MQTTClient receives a message
+  mqttClient.onMessage(onMessageReceived);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
+
+  // poll for new MQTT messages and send keep alives
+  mqttClient.poll();
+
+  // publish a message roughly every 5 seconds.
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+    char payload[512];
+    getDeviceStatus(payload);
+    sendMessage(payload);
+  }
+}
+
+unsigned long getTime() {
+  // get the current time from the WiFi module  
+  return WiFi.getTime();
+}
+
+void connectWiFi() {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.print(ssid);
+  Serial.print(" ");
+
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+}
+
+void connectMQTT() {
+  Serial.print("Attempting to MQTT broker: ");
+  Serial.print(broker);
+  Serial.println(" ");
+
+  while (!mqttClient.connect(broker, 8883)) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the MQTT broker");
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe("$aws/things/EntranceDevice/shadow/update/delta");
+  
+}
+
+void getDeviceStatus(char* payload) {
+
+  long duration, distance;                   // 각 변수를 선언합니다.
+
+  digitalWrite(trigPin, LOW);                 // trigPin에 LOW를 출력하고
+  delayMicroseconds(2);                    // 2 마이크로초가 지나면
+
+  digitalWrite(trigPin, HIGH);                // trigPin에 HIGH를 출력합니다.
+  delayMicroseconds(10);                  // trigPin을 10마이크로초 동안 기다렸다가
+
+  digitalWrite(trigPin, LOW);                // trigPin에 LOW를 출력합니다.
+
+  duration = pulseIn(echoPin, HIGH);   // echoPin핀에서 펄스값을 받아옵니다.
+  distance = duration * 17 / 1000;          //  duration을 연산하여 센싱한 거리값을 distance에 저장합니다.
+
+  // Read led status
+  //const char* led = (led1.getState() == LED_ON)? "ON" : "OFF";
+  
+  // make payload for the device update topic ($aws/things/MyMKRWiFi1010/shadow/update)
+  //sprintf(payload,"{\"state\":{\"reported\":{\"temperature\":\"%0.2f\",\"LED\":\"%s\"}}}",t,led);
+  sprintf(payload,"{\"state\":{\"reported\":{\"distance\":\"%ld\"}}}",distance);
+}
+
+void sendMessage(char* payload) {
+  char TOPIC_NAME[]= "$aws/things/EntranceDevice/shadow/update";
+  
+  Serial.print("Publishing send message:");
+  Serial.println(payload);
+  mqttClient.beginMessage(TOPIC_NAME);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+}
+
+
+void onMessageReceived(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // store the message received to the buffer
+  char buffer[512] ;
+  int count=0;
+  while (mqttClient.available()) {
+     buffer[count++] = (char)mqttClient.read();
+  }
+  buffer[count]='\0'; // 버퍼의 마지막에 null 캐릭터 삽입
+  Serial.println(buffer);
+  Serial.println();
+
+  // JSon 형식의 문자열인 buffer를 파싱하여 필요한 값을 얻어옴.
+  // 디바이스가 구독한 토픽이 $aws/things/MyMKRWiFi1010/shadow/update/delta 이므로,
+  // JSon 문자열 형식은 다음과 같다.
+  // {
+  //    "version":391,
+  //    "timestamp":1572784097,
+  //    "state":{
+  //        "LED":"ON"
+  //    },
+  //    "metadata":{
+  //        "LED":{
+  //          "timestamp":15727840
+  //         }
+  //    }
+  // }
+  //
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, buffer);
+  
+  JsonObject root = doc.as<JsonObject>();
+  JsonObject state = root["state"];
+  const char* led = state["LED"];
+  const char* limit = state["limit"];
+  Serial.println(led);
+  
+  char payload[512];
+  
+  if (strcmp(led,"ON")==0) {
+    led1.on();
+    sprintf(payload,"{\"state\":{\"reported\":{\"LED\":\"%s\"}}}","ON");
+    sendMessage(payload);
+    
+  } else if (strcmp(led,"OFF")==0) {
+    led1.off();
+    sprintf(payload,"{\"state\":{\"reported\":{\"LED\":\"%s\"}}}","OFF");
+    sendMessage(payload);
+    
+  }
+  sprintf(payload,"{\"state\":{\"reported\":{\"limit\":\"%s\"}}}",limit);
+  sendMessage(payload);
+ 
+}
